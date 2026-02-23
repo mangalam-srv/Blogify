38e80c712362076d6680a5ad938042059d4fbfc5
mangalam-srv <mangalamstp2005@gmail.com>
2026-02-23 17:12:48 +0530

    Added blog controller, routes, middleware and server updates

---
+++ server/controllers/blogController.js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js';
+
+
+export const addBlog = async( req,res)=>{
+    try{
+
+        const {title,subTitle , description , category , isPublished} =JSON.parse(req.body.blog);
+
+        const imageFile =  req.file;
+
+        //check all are present 
+        if( !title || !subTitle || !description || !category || !isPublished){
+            return res.JSON({success:false , message:"Missing required fields"})
+        }
+
+        const   fileBuffer =fs.readFileSync(imageFile.path)
+
+        //upload image to imagekit
+        const response = await imagekit.upload({
+            file:fileBuffer,
+            fileName : imageFile.originalname,
+            folder:'/blogs',
+
+        })
+
+        //optimization through imagekit url transformation 
+        const optimizedImageUrl= imagekit.url({
+            path: response.filePath,
+            transformation:[
+                {quality:'auto'},//auto compression
+                {format:'webp'},//convert to modern  format
+                {width:'1280'}//width resizing
+            ]
+        });
+
+        const image = optimizedImageUrl;
+        await Blog.create({title,subTitle,description,category,image,isPublished});
+
+        res.json({success:true , message:"Blog added successfully"})
+
+
+    }catch(error){
+        res.json({success:false , message: error.message})
+
+    }
+}
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import 'dotenv/config'
 import cors from 'cors';
 import connectDB from './configs/db.js';
-
+import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
 const app = express();
 
 await connectDB();
@@ -15,6 +16,8 @@ app.use(express.json())
 app.get('/',(req,res)=>{
      res.send("API is working")
 })
+app.use('/api/admin',adminRouter);
+app.use('/api/blog',blogRouter);
 
 
 const PORT=process.env.PORT || 3000;
