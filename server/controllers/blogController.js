c5c47e6fe72060e2d1cc975e90d8615ea1dc9cb1
mangalam-srv <mangalamstp2005@gmail.com>
2026-02-24 10:55:54 +0530

    backend routes done

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import imagekit from '../configs/imageKit.js';
 import Blog from '../models/Blog.js';
+import Comment from '../models/Comment.js';
 
 
 export const addBlog = async( req,res)=>{
@@ -86,6 +87,8 @@ export const deleteBlogById = async(req,res)=>{
         const { id } = req.body;
          await Blog.findByIdAndDelete(id);
 
+        //delete all comment assosiated with this blog
+        await Comment.deleteMany({blog:id});
         
 
         res.json({success:true ,message:"Blog deleted successfully"})
